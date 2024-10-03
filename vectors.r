216e5b1a5be4a7f444aba04701eab33aa538fcc8
Nimantha Supun <supunnimantha199@gmail.com>
2024-10-03 18:11:03 +0530

    R-vectors

---
+++ vectors.r
@@ -0,0 +1,34 @@
+
+# Cretating vector 
+# vector of string
+
+language <- c("Python", "java", "dart")
+language
+
+# vectors of numerical values
+
+numbers <- c(1, 2, 3)
+numbers
+
+
+
+# find to how many items a vectors 
+length(language)
+
+
+# sort vector
+sort(language)
+sort(numbers)
+
+
+# Access the first item(python)
+language[1]
+
+
+# Access the first and second items
+language[c(1,2)]
+
+# change item
+language[1] <- "C#"
+language
+
